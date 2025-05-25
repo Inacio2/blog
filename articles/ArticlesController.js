d98d1cac53a81e5aae70879869dc56b60c6ecbbf
Inácio da Silva Bondo <inaciosilva.bondo@gmail.com>
2025-05-25 18:40:16 +0100

    sistema de exclusão de artigos

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -4,8 +4,10 @@ const Category = require("../categories/Category.js");
 const Article = require("./Article.js");
 const slugfy = require("slugify");
 
-router.get("/admin//articles",(req,res) =>{
-    Article.findAll().then(articles => {
+router.get("/admin/articles",(req,res) =>{
+    Article.findAll({
+        include : [{model : Category}]
+    }).then(articles => {
         res.render("admin/articles/index", { articles : articles});
     });
     
@@ -32,10 +34,31 @@ router.post("/articles/save",(req,res)=>{
         body : body,
         categoryId : category
     }).then(()=>{
-        res.redirect("/admin//articles");
+        res.redirect("/admin/articles");
     })
 
 })
 
+router.post("/admin/articles/detele",(req,res)=>{
+    var id = req.body.id;
+    if(id != undefined){ // se o valor for diferente de nulo
+
+        if(!isNaN(id)){ // se for um número
+
+            Article.destroy({
+                where: {
+                    id : id
+                } 
+            }).then(()=>{ // Se não um número
+                res.redirect("/admin/articles");
+            })
+
+        }else{
+            res.redirect("/admin/articles/");
+        }
+    }else{
+        res.redirect("/admin/articles/");
+    }
+})
 
 module.exports = router
