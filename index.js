4b1a9666292056401628c7688776a8cfacbb1e2c
Inácio da Silva Bondo <inaciosilva.bondo@gmail.com>
2025-05-26 19:59:04 +0100

    criação da página incial
    exibição dos artigos na página incial

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const articleController = require("./articles/ArticlesController");
 
 const Article = require("./articles/Article");
 const Category = require("./categories/Category");
+const { where } = require("sequelize");
 
 // View Engine
 app.set('view engine','ejs');
@@ -33,9 +34,32 @@ app.use("/",categoriesController);
 app.use("/",articleController);
 
 app.get("/",(req,res)=>{
-    res.render("index")
+    Article.findAll({
+        order: [
+            ['id','DESC']
+        ]
+    }).then(articles => {
+        res.render("index", {articles:articles});
+    })
+    
 });
 
+app.get("/:slug",(req,res) =>{
+    var slug = req.params.slug
+    Article.findOne({
+        where : {
+            slug : slug
+        }
+    }).then(article =>{
+        if(article != undefined){
+            res.render("article", {article : article});
+        }else{
+            res.redirect("/");
+        }
+    }).catch(err =>{
+        res.redirect("/");
+    })
+})
 
 
 app.listen(8080,()=>{
